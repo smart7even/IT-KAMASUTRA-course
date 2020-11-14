6247fba0ec0feedcff178be9542ec5f5ecb3f839
smart7even <69794332+smart7even@users.noreply.github.com>
2020-11-14 17:01:13 +0300

    Deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
@@ -18,8 +17,3 @@ let rerenderEntireTree = () => {
 rerenderEntireTree(store.getState())
 
 store.subscribe(rerenderEntireTree)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
